3f3f9750c7392bea734b8faee4cf5f73b8977237
TheTzdDark <timur.saitbattalow@yandex.ru>
2026-06-17 09:14:38 +0500

    Update RFG Apply spotless

--- src/main/java/gregtech/api/metatileentity/multiblock/RecipeMapMultiblockController.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/RecipeMapMultiblockController.java
@@ -39,9 +39,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
