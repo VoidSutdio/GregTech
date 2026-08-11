171b981d20f27641cd306280bd5a246c21f28221
WALHAY <walhayggwp@gmail.com>
2026-08-11 19:47:38 +0300

    Add placeholders for dual hatches overlay

--- src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityDualHatch.java
+++ src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityDualHatch.java
@@ -294,8 +294,8 @@ public class MetaTileEntityDualHatch extends MetaTileEntityMultiblockNotifiableP
         super.renderMetaTileEntity(renderState, translation, pipeline);
         SimpleOverlayRenderer renderer = isExportHatch ? Textures.PIPE_OUT_OVERLAY : Textures.PIPE_IN_OVERLAY;
         renderer.renderSided(getFrontFacing(), renderState, translation, pipeline);
-        SimpleOverlayRenderer overlay = isExportHatch ? Textures.ITEM_HATCH_OUTPUT_OVERLAY :
-                Textures.ITEM_HATCH_INPUT_OVERLAY;
+        SimpleOverlayRenderer overlay = isExportHatch ? Textures.DUAL_HATCH_OUTPUT_OVERLAY :
+                Textures.DUAL_HATCH_INPUT_OVERLAY;
         overlay.renderSided(getFrontFacing(), renderState, translation, pipeline);
     }
 
